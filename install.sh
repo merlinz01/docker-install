6b6839269d86cd6fd632717f3ba608b8db03665f
Merlin <158784988+merlinz01@users.noreply.github.com>
2026-05-19 13:22:11 -0400

    Reorder lines in deb822 APT configuration

--- install.sh
+++ install.sh
@@ -569,7 +569,7 @@ do_install() {
 						echo "# WARNING: An existing Docker repository configuration using the old format was found at /etc/apt/sources.list.d/docker.list."
 						echo "# Please remove this file to avoid conflicting APT configuration."
 					fi
-					apt_repo="Types: deb\nArchitectures: $(dpkg --print-architecture)\nURIs: $DOWNLOAD_URL/linux/$lsb_dist\nSuites: $dist_version\nComponents: $CHANNEL\nSigned-By: /etc/apt/keyrings/docker.asc"
+					apt_repo="Types: deb\nURIs: $DOWNLOAD_URL/linux/$lsb_dist\nSuites: $dist_version\nComponents: $CHANNEL\nArchitectures: $(dpkg --print-architecture)\nSigned-By: /etc/apt/keyrings/docker.asc"
 					apt_repo_file="/etc/apt/sources.list.d/docker.sources"
 				else
 					apt_repo="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] $DOWNLOAD_URL/linux/$lsb_dist $dist_version $CHANNEL"
